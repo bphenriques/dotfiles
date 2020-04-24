d36d9d0c63d81b9a6c4039eefbe02ba69ae4f868
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-04-25 00:07:47 +0100

    Remove oh-my-zsh as it is not used

--- zsh/installer.oh-my-zsh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# TODO more reliable way. Or the installation script could be idempotent... :shrug:
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    chsh -s "$(which zsh)"    
-fi
--- zsh/installer.zsh-auto-completion.sh
+++ zsh/installer.zsh-auto-completion.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-# Setup fzf for zsh
+# Setup fzf for zsh. Fortunately, it is idempotent (checks if the line exists in the configuration file)
 "$(brew --prefix)/opt/fzf/install" --xdg --key-bindings --completion --update-rc --no-bash --no-fish
