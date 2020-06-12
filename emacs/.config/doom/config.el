117524c6a4665404990b1895f11dd216636817a8
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-06-12 21:02:07 +0100

    Update org-capture and remove some automations

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -12,14 +12,3 @@
 
 (setq undo-limit 80000000                         ; Raise undo-limit to 80Mb
       evil-want-fine-undo t)                      ; By default while in insert all changes are one big blob. Be more granular
-
-
-;
-(setq evil-vsplit-window-right t
-      evil-split-window-below t)
-
-(defadvice! prompt-for-buffer (&rest _)
-  :after '(evil-window-split evil-window-vsplit)
-  (+ivy/switch-buffer))
-
-(setq +ivy-buffer-preview t)
--- emacs/.config/doom/org-capture.el
+++ emacs/.config/doom/org-capture.el
@@ -6,54 +6,57 @@
 (setq org-directory "~/org/")
 (setq org-knowledge-base-repository "~/Documents/repos/knowledge-base")
 (setq org-knowledge-base-directory (concat (file-name-as-directory org-knowledge-base-repository) "org"))
+(setq org-knowledge-exported-content-directory (concat (file-name-as-directory org-knowledge-base-directory) "content"))
 
 ;;;
 ;;; Knowledge base
 ;;;
-(defvar knowledge-base-capture-mode nil)
-(defvar knowledge-base-capture-target-section nil)
-(defvar knowledge-base-capture-entry-title nil)
 
-(defun knowledge-base/org-capture/prepare ()
-  "Required advice in order to share information between the function that returns the file and the function that returns the template"
+(defun knowledge-base/new-entry/file ()
+  (let ((title (org-capture-get :knowledge-base-title))
+        (section (org-capture-get :knowledge-base-section)))
+
+    (concat (file-name-as-directory org-knowledge-base-directory)
+            (format "%s/%s.org" (org-hugo-slug section) (org-hugo-slug title)))))
+
+(defun knowledge-base/new-entry/template ()
   (let* ((sections (custom/list-directories org-knowledge-base-directory))
-         (selected-section (completing-read "Section: " sections nil 'confirm)))
+         (section (completing-read "Section: " sections nil t)) ; guarantee one option is selected
+         (title (read-string "Title: ")))
 
-    (defun new-section ()
-      (setq knowledge-base-capture-mode "create-section"))
+    (if (equal title "")
+        (error "Empty title!")
+      (progn
+        (org-capture-put :knowledge-base-section section :knowledge-base-title title)
+        (get-string-from-file (concat (file-name-as-directory org-knowledge-base-repository) "template-entry.org"))))))
 
-    (defun new-entry ()
-      (setq knowledge-base-capture-mode "create-entry")
-      (let ((title (read-string "Title: ")))
-        (when (not (equal title ""))
-          (setf knowledge-base-capture-entry-title title))))
+(defun knowledge-base/new-section/template ()
+  (let* ((sections (custom/list-directories org-knowledge-base-directory))
+         (section (completing-read "Section: " sections)))
 
-    (setq knowledge-base-capture-target-section selected-section)
-    (cond ((member selected-section sections) (new-entry))
-          ((y-or-n-p (format "Section '%s' does not exist! Create it?" selected-section)) (new-section))
-          (t nil))))
+    (if (member section sections)
+        (error (concat "Section '%s' already exists! Skipping." section))
+      (progn
+        (org-capture-put :knowledge-base-section section)
+        (get-string-from-file (concat (file-name-as-directory org-knowledge-base-repository) "template-section.org"))))))
 
-(defun knowledge-base/org-capture/target-file ()
-  (let ((mode knowledge-base-capture-mode)
-        (section knowledge-base-capture-target-section)
-        (title knowledge-base-capture-entry-title))
+(defun knowledge-base/new-section/file ()
+  (let ((section (org-capture-get :knowledge-base-section)))
+    (concat (file-name-as-directory org-knowledge-base-directory)
+            (format "%s/_index.org" (org-hugo-slug section)))))
 
-  (concat (file-name-as-directory org-knowledge-base-directory)
-          (cond ((equal mode "create-section")
-                 (format "%s/_index.org" (org-hugo-slug section)))
-                ((equal mode "create-entry")
-                 (format "%s/%s.org" (org-hugo-slug section) (org-hugo-slug title)))
-                (t nil)))))
+(defun knowledge-base/new-fleeting-entry/template ()
+  (let* ((title (read-string "Title: ")))
+    (if (equal title "")
+        (error "Empty title!")
+      (progn
+        (org-capture-put :knowledge-base-title title)
+        (get-string-from-file (concat (file-name-as-directory org-knowledge-base-repository) "template-entry.org"))))))
 
-(defun knowledge-base/org-capture/template ()
-  (knowledge-base/org-capture/prepare)
-  (let ((mode knowledge-base-capture-mode))
-    (cond ((equal mode "create-entry")
-           (get-string-from-file (concat (file-name-as-directory org-knowledge-base-repository) "template-entry.org")))
-          ((equal mode "create-section")
-           (get-string-from-file (concat (file-name-as-directory org-knowledge-base-repository) "template-section.org")))
-          (t nil))))
-
+(defun knowledge-base/new-fleeting-entry/file ()
+  (let ((title (org-capture-get :knowledge-base-title)))
+    (concat (file-name-as-directory org-knowledge-base-directory)
+            (format "%s.org" (org-hugo-slug title)))))
 
 ;;;
 ;;; Org Capture
@@ -61,11 +64,10 @@
 
 (after! org-capture
   (setq org-capture-templates
-        '(("f" "Fleeting note" entry (file "~/Documents/repos/knowledge-base/org/fleeting-notes.org")
-           "* %?" :empty-lines 1)
-          ("k" "New [k]nowledge base entry" plain
-           (file knowledge-base/org-capture/target-file)
-           (function knowledge-base/org-capture/template))
-         )
-    )
-  )
+        '(
+          ("k" "Knowledge Base")
+          ("ks" "New [s]ection" plain (file knowledge-base/new-section/file) (function knowledge-base/new-section/template) :immediate-finish t)
+          ("ke" "New [e]ntry" plain (file knowledge-base/new-entry/file) (function knowledge-base/new-entry/template))
+          ("kf" "New [f]leeting entry" plain (file knowledge-base/new-fleeting-entry/file) (function knowledge-base/new-fleeting-entry/template))
+          )
+        ))
