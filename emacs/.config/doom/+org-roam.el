41bc827cede6307e269ec313c4e5c14a3754a407
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-06-13 22:09:42 +0100

    Enable org-roam

---
+++ emacs/.config/doom/+org-roam.el
@@ -0,0 +1,3 @@
+;;; ~/Documents/repos/workspace/emacs/.config/doom/+org-roam.el -*- lexical-binding: t; -*-
+
+(setq org-roam-graph-viewer "open")
--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -8,7 +8,7 @@
 (load! "+theme")
 (load! "+macos")
 (load! "org-capture")
-
+(load! "+org-roam")
 
 (setq undo-limit 80000000                         ; Raise undo-limit to 80Mb
       evil-want-fine-undo t)                      ; By default while in insert all changes are one big blob. Be more granular
