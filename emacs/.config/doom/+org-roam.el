28fee6c39dc08c743dfbf6782d729ce49a909585
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-08-29 11:04:13 +0100

    feat: Setup org-roam-server

--- emacs/.config/doom/+org-roam.el
+++ emacs/.config/doom/+org-roam.el
@@ -4,17 +4,22 @@
 (require 'cl)
 
 (setq
- org-roam-tag-sources '(prop last-directory)
- org-roam-graph-viewer "open")
-
-(setq
  org-knowledge-base-repository "~/Documents/repos/knowledge-base"
  org-knowledge-base-directory (concat (file-name-as-directory org-knowledge-base-repository) "org")
  org-knowledge-base-section-template (concat (file-name-as-directory org-knowledge-base-repository) "template-section.org")
- org-knowledge-base-entry-template (concat (file-name-as-directory org-knowledge-base-repository) "template-entry.org"))
+ org-knowledge-base-entry-template (concat (file-name-as-directory org-knowledge-base-repository) "template-entry.org")
+
+ ; Org-roam set globally to take notes anywhere.
+ ; - Due to https://github.com/org-roam/org-roam-server/issues/8 org-roam-server does not respect .dir-locals.el
+ org-roam-directory org-knowledge-base-directory
+ org-roam-file-exclude-regexp "_index.org"
+ org-roam-tag-sources '(prop)
+
+ ; Org-roam-server
+ org-roam-server-port 8090)
 
 ;;;
-;;; Knowledge base
+;;; Org-Roam Capture
 ;;;
 
 (defun knowledge-base/prompt-section ()
@@ -39,15 +44,38 @@
 
 (add-hook 'org-capture-after-finalize-hook 'knowledge-base/setup-section-if-needed)
 
-;;;
-;;; Org Capture
-;;;
 (after! org-roam
-  (setq org-roam-capture-templates
+ (setq org-roam-capture-templates
         `(
           ("d" "default" plain (function org-roam--capture-get-point)
            "%?"
            :file-name "%(knowledge-base/prompt-section)/${slug}"
            :head ,(get-string-from-file org-knowledge-base-entry-template)
            :unnarrowed t)
-        )))
+          )))
+
+;;;
+;;; Org-Roam Server
+;;;
+;;; Requires Org-Server Protocol
+;;;
+
+(defun org-roam-server-open ()
+  "Ensure the server is active, then open the roam graph."
+  (interactive)
+  (when (eq org-roam-server-mode nil)
+    (org-roam-server-mode 1))
+  (call-process "open" nil nil nil (format "http://%s:%d" org-roam-server-host org-roam-server-port)))
+
+
+;;;
+;;; Key-Bindings
+;;;
+
+(after! org-roam
+  (map! :leader
+         (:prefix-map ("k" . "knowledge-base")
+          :desc "Roam Insert"    "i"    #'org-roam-insert
+          :desc "Roam Find"      "f"    #'org-roam-find-file
+          :desc "Roam Server"    "g"    #'org-roam-server-open
+          :desc "Roam Capture"   "c"    #'org-roam-capture)))
--- emacs/.config/doom/packages.el
+++ emacs/.config/doom/packages.el
@@ -1,2 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
+
+(package! org-roam-server)
