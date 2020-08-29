96310c10173068bcc606f71cdca8473adf032edc
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-08-29 17:06:04 +0100

    org-mode: Base settings

--- emacs/.config/doom/+org-mode.el
+++ emacs/.config/doom/+org-mode.el
@@ -1,9 +1,8 @@
 ;;; ../../Documents/repos/dotfiles/emacs/.config/doom/+org-mode.el -*- lexical-binding: t; -*-
 
-(setq
- org-directory "~/org/"
- org-hide-emphasis-markers t
- org-pretty-entities t
- org-fontify-whole-heading-line t
- org-fontify-done-headline t
- org-fontify-quote-and-verse-blocks t)
+(after! org
+  (setq
+   org-directory "~/org/"
+   org-ellipsis "▼"                        ; Icon for collapsed items
+   org-hide-emphasis-markers t             ; Hide markers
+))
