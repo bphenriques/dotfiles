7a7c6b32efd5eb8e40b6d23e01f910d51bea3355
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-09-23 23:48:11 +0100

    Add plantuml

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -147,7 +147,7 @@
        ;;ocaml             ; an objective camel
        (org +hugo +roam)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
-       ;;plantuml          ; diagrams for confusing people more
+       plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
