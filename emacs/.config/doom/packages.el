6bed381e9ee8d56fb544a34bcfd74f9703e9333f
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-09-30 22:45:52 +0100

    Pin some emacs packages versions to newer versions

--- emacs/.config/doom/packages.el
+++ emacs/.config/doom/packages.el
@@ -1,4 +1,11 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(package! org-roam-server)
+(package! ox-hugo
+  :recipe (:host github :repo "kaushalmodi/ox-hugo") :pin "95723cd")
+
+(package! org-roam
+  :recipe (:host github :repo "org-roam/org-roam") :pin "369753c")
+
+(package! org-roam-server
+  :recipe (:host github :repo "org-roam/org-roam-server") :pin "8d1d143")
