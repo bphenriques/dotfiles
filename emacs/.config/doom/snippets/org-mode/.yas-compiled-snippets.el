63eaed0c8edfc41fafd8de4bf0e8e1cda1940f5e
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-10-03 21:52:57 +0100

    emacs: Add custom snippets

---
+++ emacs/.config/doom/snippets/org-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `org-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'org-mode
+                     '(("<plantuml" "#+BEGIN_SRC plantuml :file $1 :exports results :eval never-export\n  $0\n#+END_SRC\n" "plantuml" nil nil nil "/Users/bruno.henriques/.config/doom/snippets/org-mode/plantuml" nil nil)))
+
+
+;;; Do not edit! File generated at Sat Oct  3 21:47:26 2020
