f42389f09209b0bc8c4b0e5f90fd1964fe72e153
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-11-08 23:31:56 +0000

    emacs: finetune

--- emacs/.config/doom/+org-mode.el
+++ emacs/.config/doom/+org-mode.el
@@ -5,4 +5,5 @@
    org-directory "~/org/"
    org-ellipsis "▼"                        ; Icon for collapsed items
    org-hide-emphasis-markers t             ; Hide markers
+   org-startup-with-inline-images nil      ; do not inline images by default
 ))
--- emacs/.config/doom/packages.el
+++ emacs/.config/doom/packages.el
@@ -5,7 +5,7 @@
   :recipe (:host github :repo "kaushalmodi/ox-hugo") :pin "95723cd")
 
 (package! org-roam
-  :recipe (:host github :repo "org-roam/org-roam") :pin "a7cf48e")
+  :recipe (:host github :repo "org-roam/org-roam") :pin "7602b8c48d8c5077a93b533066db1127ebfce597")
 
 (package! org-roam-server
-  :recipe (:host github :repo "org-roam/org-roam-server") :pin "1dc94e102d60e53bb9929b1cdc55d4d8c2b0d64f")
+  :recipe (:host github :repo "org-roam/org-roam-server") :pin "fde2636d794f020ed5810fa38fe57a264932f661")
