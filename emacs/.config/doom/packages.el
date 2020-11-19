62b588b8e713a4ab303e67c2b8b35b760c9d94f1
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-11-19 19:14:16 +0000

    bump org-roam

--- emacs/.config/doom/packages.el
+++ emacs/.config/doom/packages.el
@@ -5,7 +5,7 @@
   :recipe (:host github :repo "kaushalmodi/ox-hugo") :pin "95723cd")
 
 (package! org-roam
-  :recipe (:host github :repo "org-roam/org-roam") :pin "7602b8c48d8c5077a93b533066db1127ebfce597")
+  :recipe (:host github :repo "org-roam/org-roam") :pin "1b598a461849318fca2df3917c254d8ee483a87e")
 
 (package! org-roam-server
   :recipe (:host github :repo "org-roam/org-roam-server") :pin "fde2636d794f020ed5810fa38fe57a264932f661")
