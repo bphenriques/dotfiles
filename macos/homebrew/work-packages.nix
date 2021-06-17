dbb87c4c2341c07a92f40fae14406493091dcbbd
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2021-06-17 13:42:41 +0100

    feat: Add work packages

--- macos/homebrew/work-packages.nix
+++ macos/homebrew/work-packages.nix
@@ -2,7 +2,13 @@
   homebrew.enable = true;
   
   homebrew.taps = [ "homebrew/cask" ];
+  homebrew.brews = [
+    "helm" 
+    "python@3.9"
+    "terraform"
+  ];
   homebrew.casks = [
+    "google-cloud-sdk"
     "slack"             # Communication. Oh god.. you can be annoying.
   ];
 }
