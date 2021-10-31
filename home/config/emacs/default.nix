6c42dd68a17b4872ff6d122cc6b716579066bc0a
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2021-10-31 21:50:57 +0000

    Migrate homebrew pngpaste to plain nix derivation

--- home/config/emacs/default.nix
+++ home/config/emacs/default.nix
@@ -1,8 +1,30 @@
 { config, lib, pkgs, ... }:
 
+let
+  # See options here: https://github.com/nix-community/emacs-overlay
+  # https://emacs.stackexchange.com/questions/33065/on-linux-why-should-one-choose-lucid-over-gtk-gui-for-emacs
+  # Still not sure if I am going to be full-blown terminal for Emacs.
+  emacs-package = if pkgs.stdenv.isDarwin then pkgs.emacsGcc else pkgs.emacsPgtkGcc;
+
+  # Pasting images to emacs
+  pngpaste = pkgs.stdenv.mkDerivation rec {
+    src = pkgs.fetchFromGitHub {
+      owner = "jcsalterego";
+      repo="pngpaste";
+      rev="67c39829fedb97397b691617f10a68af75cf0867";
+      sha256="089rqjk7khphs011hz3f355c7z6rjd4ydb4qfygmb4x54z2s7xms";
+    };
+    name = "pngpaste";
+    buildInputs = [pkgs.darwin.apple_sdk.frameworks.Cocoa];
+    installPhase = ''
+      mkdir -p $out/bin
+      cp pngpaste $out/bin/
+    '';
+  };
+in
 {
   home.packages = with pkgs; [
-    emacsGcc     # See options here: https://github.com/nix-community/emacs-overlay
+    emacs-package
 
     # Doom emacs dependencies
     # https://github.com/hlissner/doom-emacs
@@ -14,8 +36,9 @@
     # Modules dependencies
     sqlite      # Org Roam.
     plantuml    # Plant UML.
-
-    # For nixOS
+  ] ++ lib.optionals pkgs.stdenv.isDarwin [
+    pngpaste
+  ] ++ lib.optionals pkgs.stdenv.isLinux [
     (makeDesktopItem {
       name = "org-protocol";
       exec = "emacsclient %u";
--- macos/emacs/default.nix
+++ macos/emacs/default.nix
@@ -4,7 +4,6 @@
   ];
 
   # Setups
-  # - Emacs GUI app has to be copied manually (https://github.com/d12frosted/homebrew-emacs-plus/issues/41).
   # - Org-Protocol
   system.activationScripts.postUserActivation.text = ''
     echo "Copying org-protocol.app to /Applications"
