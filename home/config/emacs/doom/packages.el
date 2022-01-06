5adf39f4277a4fe45da84dc880a5c428d43f030b
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2022-01-06 21:53:48 +0000

    Bump ox-hugo

--- home/config/emacs/doom/packages.el
+++ home/config/emacs/doom/packages.el
@@ -5,7 +5,7 @@
   :recipe (:host github :repo "hasu/notdeft"))
 
 (package! ox-hugo
-  :recipe (:host github :repo "kaushalmodi/ox-hugo") :pin "de5dfdfdcdc3ec50cb27d1f6cf7314a5c9aa1385")
+  :recipe (:host github :repo "kaushalmodi/ox-hugo") :pin "458142675bb5a0e7ee26ecea07d75c10aa52184b")
 
 (package! org-roam
   :recipe (:host github :repo "org-roam/org-roam") :pin "679ef6ef001fd1a69b691108178721aa913e7f0f")
