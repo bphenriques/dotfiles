ad07054f7863e9e4a6e03189c9fa46d1d206ffeb
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2022-02-17 22:13:22 +0000

    Remove unneded dependencies

--- home/config/zsh/aliases.zsh
+++ home/config/zsh/aliases.zsh
@@ -12,8 +12,6 @@ alias e="$EDITOR"
 
 # Java
 alias javals='/usr/libexec/java_home -V'
-alias java8='export JAVA_HOME=$(/usr/libexec/java_home -v1.8)'
-alias java11='export JAVA_HOME=$(/usr/libexec/java_home -v11)'
 
 # Nix
 alias nix-cleanup='nix-collect-garbage -d --delete-older-than 30d'
