43497eec9636c75bcd622afd607bbfe83bc00a30
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2022-02-20 18:09:15 +0000

    Bump packages

--- home/config/emacs/doom/packages.el
+++ home/config/emacs/doom/packages.el
@@ -5,13 +5,13 @@
   :recipe (:host github :repo "hasu/notdeft"))
 
 (package! ox-hugo
-  :recipe (:host github :repo "kaushalmodi/ox-hugo") :pin "458142675bb5a0e7ee26ecea07d75c10aa52184b")
+  :recipe (:host github :repo "kaushalmodi/ox-hugo") :pin "f0d06bdb8a2f6256f0e71e085e2761ea8e0c3a5c")
 
 (package! org-roam
-  :recipe (:host github :repo "org-roam/org-roam") :pin "679ef6ef001fd1a69b691108178721aa913e7f0f")
+  :recipe (:host github :repo "org-roam/org-roam") :pin "d20480bb8d86764d0ce37906fcda0484b46ee7fa")
 
 (package! org-roam-ui
-  :recipe (:host github :repo "org-roam/org-roam-ui") :pin "9ed0c5705a302a91fab2b8bcc777a12dcf9b3682")
+  :recipe (:host github :repo "org-roam/org-roam-ui") :pin "309fe3c58c7081de4e2c9c64f7b40ea291926048")
 
 (package! git-auto-commit-mode
   :recipe (:host github :repo "ryuslash/git-auto-commit-mode") :pin "a6b6e0fa183be381463e2b44ef128db1b6c4234b")
