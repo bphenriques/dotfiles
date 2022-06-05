98a89d5c02c3b14eebf9ff60b5cb9a2b43280dc1
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2022-06-05 17:15:46 +0100

    minor revision

--- home/common.nix
+++ home/common.nix
@@ -1,29 +1,36 @@
 { config, pkgs, lib, ... }:
 {
   home.packages = with pkgs; [
-    coreutils   # Consistency across different OS.
-    findutils   # Consistent find and xargs across different OS.
-    watch       # Commands on loop.
-    tree        # File navigation.
+    # Consistent UNIX command line tools
+    coreutils
+    findutils
+    watch
+    tree
+    parallel
+
+    # Search
     ripgrep     # Alternative to grep. Use with `rg`.
     fzf         # Fuzzy search.
     jq          # Query JSON.
-    bat         # Preview with code highlight.
     fd          # A better `find`.
+    tealdeer    # Faster `tldr`.
+
+    # Security
     gnupg       # To manage GNUPG keys.
     pinentry    # To input keys.
-    wget        # Download stuff.
 
-    # Other tools that I use from time to time:
+    # Web
+    wget        # Download stuff.
     httpie      # Alternative to curl. Use with `http`.
     ngrok       # Tunneling.
-    parallel    # Useful to parallelize tasks.
 
-    # Tools I am experimenting:
+    # Other
+    bat         # Preview with code highlight.
+
+    # Experimenting with:
     procs       # Fancy `ps`.
     htop        # Fancy `top`.
     exa         # Fancy `ls`.
-    tealdeer    # Faster `tldr`.
     yq          # Query YAML. Similar to JQ.
   ];
 
@@ -34,7 +41,7 @@
     ./config/kitty
     ./config/zsh
     ./config/fzf
-    ./config/tmux    
+    ./config/tmux
   ];
 
   home.stateVersion = "22.05";
