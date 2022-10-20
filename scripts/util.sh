f213e623d8ed63fdc69bd65cd8a71fd84c275ce7
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2022-10-20 10:55:03 +0100

    feat: align info/warn/error msgs

--- scripts/util.sh
+++ scripts/util.sh
@@ -2,12 +2,12 @@
 
 info() {
     # shellcheck disable=SC2059
-    printf '\r  [ \033[00;34m..\033[0m ] %s\n' "$1"
+    printf '\r  [  \033[00;34m..\033[0m  ] %s\n' "$1"
 }
 
 success() {
     # shellcheck disable=SC2059
-    printf '\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n' "$1"
+    printf '\r\033[2K  [  \033[00;32mOK\033[0m  ] %s\n' "$1"
 }
 
 warn() {
