b1e66820f641dff20784aefb42d961f5b4a3ec06
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2022-10-20 23:26:03 +0100

    minor touches

--- home/config/emacs/default.nix
+++ home/config/emacs/default.nix
@@ -1,16 +1,16 @@
 { config, lib, pkgs, ... }:
 
 let
-  # Pasting images to emacs
+  # MacOS: Pasting images to emacs
   pngpaste = pkgs.stdenv.mkDerivation rec {
+    name = "pngpaste";
+    buildInputs = [pkgs.darwin.apple_sdk.frameworks.Cocoa];
     src = pkgs.fetchFromGitHub {
       owner = "jcsalterego";
       repo="pngpaste";
       rev="67c39829fedb97397b691617f10a68af75cf0867";
       sha256="089rqjk7khphs011hz3f355c7z6rjd4ydb4qfygmb4x54z2s7xms";
     };
-    name = "pngpaste";
-    buildInputs = [pkgs.darwin.apple_sdk.frameworks.Cocoa];
     installPhase = ''
       mkdir -p $out/bin
       cp pngpaste $out/bin/
--- home/config/git/default.nix
+++ home/config/git/default.nix
@@ -1,6 +1,5 @@
 { config, lib, pkgs, ... }:
 
-
 {
   home.packages = with pkgs; [
     git                             # Should be already installed.
