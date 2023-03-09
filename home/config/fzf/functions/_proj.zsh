89534faada2c3aed91ded7793d143cff8fb58de2
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2023-03-09 23:07:41 +0000

    fix basename for both macos and nixos

--- home/config/fzf/functions/_proj.zsh
+++ home/config/fzf/functions/_proj.zsh
@@ -1,5 +1,5 @@
 #compdef proj
-project="$(find "$WORKSPACE" -type d -mindepth 1 -maxdepth 1 -print0 | xargs -0 basename | sort | fzf --layout=reverse)"
+project="$(find "$WORKSPACE" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -I {} -0 basename {} | sort | fzf --layout=reverse)"
 compadd "$project"
 
 # Workaround to make fzf work for completion definitions. Based on https://github.com/lincheney/fzf-tab-completion.
