6b92f288adac07be8deb5808b0189059acc56195
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2023-08-16 21:02:54 +0100

    Revert "Minor tidyup to the outputs"
    This reverts commit d0aa5e522403d035d8af43c9c7bc3b4c34f96639.

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "bphenriques's Nix configuration for his machines";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";               # Default to stable for most things.
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable"; # Unstable for some packages.
 
     darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";             # Pin Darwin to unstable.
 
     home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";       # Pin Home-Manager to unstable.
   };
 
   outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, darwin, home-manager, ... }:
@@ -37,28 +37,26 @@
         };
       };
 
-      commonLib = import ./lib/common.nix { lib = nixpkgs-unstable.lib; };
-
       nixosLib = import ./lib/nixos.nix {
-        inherit home-manager nixpkgsConfig nixConfig;   # Modules and configurations.
-        nixpkgs = nixpkgs-unstable;                     # Requires specific stage of nixpkgs.
-        nixosModules = self.modules.nixos;              # Custom nixos modules.
-        homeManagerModules = self.modules.homeManager;  # Custom home-manager modules.
-        lib = nixpkgs-unstable.lib;                     # Requires specific stage of nixpkgs.
+        inherit home-manager nixpkgsConfig nixConfig; # Modules and configurations.
+        nixpkgs = nixpkgs-unstable;                   # Requires specific stage of nixpkgs.
+        nixosModules = self.nixosModules;             # Custom nixos modules.
+        homeManagerModules = self.homeManagerModules; # Custom home-manager modules.
+        lib = nixpkgs-unstable.lib;                   # Requires specific stage of nixpkgs.
       };
 
       macosLib = import ./lib/macos.nix {
         inherit darwin home-manager nixpkgsConfig nixConfig;  # Modules and configurations.
-        darwinModules = self.modules.darwin;                  # Custom darwin modules.
-        homeManagerModules = self.modules.homeManager;        # Custom home-manager modules.
+        darwinModules = self.darwinModules;                   # Custom darwin modules.
+        homeManagerModules = self.homeManagerModules;         # Custom home-manager modules.
         lib = nixpkgs-unstable.lib;                           # Requires specific stage of nixpkgs.
       };
 
       homeManagerLib = import ./lib/home-manager.nix {
-        inherit home-manager nixpkgsConfig;             # Modules and configurations.
-        nixpkgs = nixpkgs-unstable;                     # Requires specific stage of nixpkgs.
-        homeManagerModules = self.modules.homeManager;  # Custom home-manager modules.
-        lib = nixpkgs-unstable.lib;                     # Requires specific stage of nixpkgs.
+        inherit home-manager nixpkgsConfig;           # Modules and configurations.
+        nixpkgs = nixpkgs-unstable;                   # Requires specific stage of nixpkgs.
+        homeManagerModules = self.homeManagerModules; # Custom home-manager modules.
+        lib = nixpkgs-unstable.lib;                   # Requires specific stage of nixpkgs.
       };
     in {
       # No alias is required: nixos-rebuild looks for the right configurating under nixosConfigurations by default.
@@ -71,10 +69,8 @@
       };
 
       # Custom modules. Either adds new feature or redefines functionality to have finer grain control over the output.
-      modules = {
-        nixos = import ./nixos/modules;       # Custo
-        homeManager = import ./home/modules;
-        darwin = import ./darwin/modules;
-      };
+      nixosModules        = import ./nixos/modules;
+      homeManagerModules  = import ./home/modules;
+      darwinModules       = import ./darwin/modules;
     };
 }
