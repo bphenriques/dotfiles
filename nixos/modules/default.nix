9aa58c138cbe6b7c80e9f994a2fff3b7f2fd4f13
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2023-08-19 11:49:59 +0100

    Remove jellyfin (cant use everywhere)

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,6 +1,5 @@
 {
   bphenriques-user                              = ./user.nix;
-  bphenriques-services-jellyfin                 = ./services/jellyfin.nix;
   bphenriques-services-input-remapper-profiles  = ./services/input-remapper.nix;
   bphenriques-services-solaar                   = ./services/solaar.nix;
   bphenriques-services-sunshine                 = ./services/sunshine.nix;
