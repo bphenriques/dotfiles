4515fec6b3b6deaed20c5faf179cfb74b8e9c34f
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2024-01-21 11:35:48 +0000

    Partially fix #30

--- bin/update.sh
+++ bin/update.sh
@@ -5,22 +5,45 @@ SCRIPT_PATH="$(dirname "$0")"
 # shellcheck source=util.sh
 . "$SCRIPT_PATH"/util.sh
 
-info 'Nix Flake - Upgrading...'
-nix flake update
-success 'Nix Flake - Complete!'
+update_flake() {
+  info 'Nix Flake - Upgrading...'
+  nix flake update
+  success 'Nix Flake - Complete!'
+}
 
+update_homebrew() {
+  info 'Homebrew - Upgrading...'
+  if [ "$(uname -m)" = "arm64" ]; then
+    info "Homebrew - Using arm64 brew"
+    brew_bin="/opt/homebrew/bin/brew"
+  else
+    info "Homebrew - Using x86 brew"
+    brew_bin="/usr/local/bin/brew"
+  fi
+  $brew_bin upgrade && $brew_bin update
+  success 'Homebrew - Complete!'
+}
+
+update_nix_darwin_multiuser() {
+  info 'Nix Multiuser - Upgrading...'
+  sudo -i sh -c 'nix-channel --update && nix-env --install --attr nixpkgs.nix && launchctl remove org.nixos.nix-daemon && launchctl load /Library/LaunchDaemons/org.nixos.nix-daemon.plist'
+  success 'Nix Multiuser - Complete!'
+}
+
+# FIXME: https://nixos.org/manual/nix/stable/installation/upgrading#upgrading-nix
+update_nix_nixos() {
+  warn "Not supported yet. Need to figure out single-vs-multi user"
+}
+
+update_flake
 case "$(uname -s)" in
   Darwin)
-    info 'Homebrew - Upgrading...'
-    if [ "$(uname -m)" = "arm64" ]; then
-      info "Homebrew - Using arm64 brew"
-      brew_bin="/opt/homebrew/bin/brew"
-    else
-      info "Homebrew - Using x86 brew"
-      brew_bin="/usr/local/bin/brew"
-    fi
-    $brew_bin upgrade && $brew_bin update
-    success 'Homebrew - Complete!'
+    update_homebrew
+    update_nix_darwin_multiuser
     ;;
-  *) ;;
+  *)
+    if [ -d /etc/nixos ]; then
+      update_nix_nioxs
+    fi
+    ;;
 esac
