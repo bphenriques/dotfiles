e36b5cd609f7a4c86d6213b2c978fefd60b7d4fc
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2024-06-13 20:00:18 +0100

    I have little clue, copied from nixos-hardware

--- host/laptop/hardware-configuration.nix
+++ host/laptop/hardware-configuration.nix
@@ -1,15 +1,14 @@
 { config, lib, pkgs, modulesPath, ... }:
 
-# TODO: Check https://github.com/NixOS/nixos-hardware/blob/master/lenovo/legion/16aph8/default.nix
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" "amd_pstate=active" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" "amdgpu" ];
+  boot.kernelModules = [ "kvm-amd"  ];
   boot.extraModulePackages = [ ];
 
-  swapDevices = [ ]; # FIXME: might add one.
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
