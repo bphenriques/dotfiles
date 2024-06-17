c4c6a3403ed3e0d5f2d26137fd7425d5601bf5d0
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2024-06-17 21:34:25 +0100

    WIP

--- host/laptop/home.nix
+++ host/laptop/home.nix
@@ -1,4 +1,21 @@
 { config, pkgs, lib, ... }:
 {
-  
+  imports = [
+    #../../home/config
+    ../../home/config/plasma
+  ];
+  programs.plasma.workspace.wallpaper = ./wallpaper.png;
+
+  # Gpg
+  programs.gpg.enable = true;
+  services.gpg-agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-gnome3;
+  };
+
+  home.packages = with pkgs; [
+    killall     # Useful
+  ];
+
+  home.stateVersion = "24.05";
 }
