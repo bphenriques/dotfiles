6e8eabdf063356ab113c4171814da15f215d13de
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2024-06-17 21:30:07 +0100

    WIP

--- nixos/modules/programs/qbittorrent.nix
+++ nixos/modules/programs/qbittorrent.nix
@@ -17,6 +17,6 @@ in
 
   config = mkIf cfg.enable {
     environment.systemPackages = [ pkgs.qbittorrent ];
-    user.extraGroups = [ "qbittorrent" ];
+    # FIXME: user.extraGroups = [ "qbittorrent" ];
   };
 }
