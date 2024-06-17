4ae2f18e76141d860ace3fd3faff82f459532189
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2024-06-17 21:30:07 +0100

    WIP

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,5 +1,4 @@
 {
-  bphenriques-user                              = ./user.nix;
   bphenriques-services-input-remapper-profiles  = ./services/input-remapper.nix;
   bphenriques-services-solaar                   = ./services/solaar.nix;
   bphenriques-services-sunshine                 = ./services/sunshine.nix;
