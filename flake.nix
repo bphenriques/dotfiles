21c4612fcf4c34f291c3aa3a6b1b17cd060a5c0f
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2024-06-17 21:33:33 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -61,6 +61,16 @@
           dates = "weekly";
           options = "--delete-older-than 7d";
         };
+        substituters = [
+          "https://hyprland.cachix.org"
+          "https://nix-community.cachix.org"
+          "https://ghostty.cachix.org"
+        ];
+        trusted-public-keys = [
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
+        ];
       };
 
       nixConfig = {
@@ -74,17 +84,6 @@
         extraOptions = ''
           min-free = ${toString (5 * 1024 * 1024 * 1024)}
         '';
-
-        substituters = [
-          "https://hyprland.cachix.org"
-          "https://nix-community.cachix.org"
-          "https://ghostty.cachix.org"
-        ];
-        trusted-public-keys = [
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
-        ];
       };
 
       homeManagerModules = [
