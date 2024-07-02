5a055bd2ded1b9694be9539c9bb6d06492aac09f
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2024-07-02 13:52:36 +0100

    Fix logseq linutes only

--- home/config/logseq/default.nix
+++ home/config/logseq/default.nix
@@ -1,10 +1,10 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 let
   logseqDotfiles = "/home/${config.home.username}/.dotfiles/home/config/logseq";
 in
 {
   # TODO: Create logseq configurations for .logseq/settings/
-  home.packages = with pkgs; [ logseq ];
+  home.packages = lib.optionals pkgs.stdenv.isLinux [ pkgs.logseq ];
   home.file = {
     ".logseq/config/plugins.edn".source = config.lib.file.mkOutOfStoreSymlink "${logseqDotfiles}/plugins.edn";
     ".logseq/config/config.edn".source = config.lib.file.mkOutOfStoreSymlink "${logseqDotfiles}/configs.edn";
