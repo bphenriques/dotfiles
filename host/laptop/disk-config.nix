2d0123bcad895b9d1112037d2e8c48e5f59e4abc
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2024-07-04 16:21:06 +0100

    it boots...?

--- host/laptop/disk-config.nix
+++ host/laptop/disk-config.nix
@@ -33,7 +33,7 @@
            };
            ESP = {
              type = "EF00";
-             size = "512MiB";
+             size = "512M";
              content = {
                type = "filesystem";
                format = "vfat";
@@ -57,103 +57,110 @@
          };
        };
      };
-    zpool = {
-      zroot = {
-        type = "zpool";
+   };
+  zpool = {
+    zroot = {
+      type = "zpool";
 
-        # https://www.high-availability.com/docs/ZFS-Tuning-Guide/#general-recommendations
-        rootFsOptions = {
-          compression = "lz4";
-          xattr = "sa";
-          atime = "off";
-        };
-
-        options = {
-          # SSD supports 512/512 (physical/logical):
-          #   $ lsblk -t /dev/nvme0n1
-          #
-          # But.. it actually supports 4096/4096:
-          #   $ nvme id-ns -H /dev/nvme0n1
-          #
-          # 512/512 is mostly for compatability reasons and, in our case, we can set it by setting it up accordingly:
-          # $ nvme format --lbaf=1 /dev/nvme0n1
-          #
-          # https://wiki.archlinux.org/title/Advanced_Format
-          # https://www.high-availability.com/docs/ZFS-Tuning-Guide/#alignment-shift-ashiftn
-          ashift = "12";
-        };
-
-        # https://github.com/KornelJahn/nixos-disko-zfs-test/blob/main/hosts/testhost-disko.nix
-        datasets =
-          let
-            systemDatasets = {
-              system = {
-                type = "zfs_fs";
-                options.mountpoint = "none";
-              };
-              "system/root" = {
-                type = "zfs_fs";
-                mountpoint = "/";
-                options.mountpoint = "legacy";
-                postCreateHook = ''zfs snapshot zroot/system/root@blank'';
-              };
-              "system/nix" = {
-                type = "zfs_fs";
-                mountpoint = "/nix";
-                options.mountpoint = "legacy";
-              };
-              "system/log" = {
-                type = "zfs_fs";
-                mountpoint = "/var/log";
-                options.mountpoint = "legacy";
-              };
-              "system/persist" = {
-                type = "zfs_fs";
-                mountpoint = "/persist/system";
-                options.mountpoint = "legacy";
-              };
-              "system/cache" = {
-                type = "zfs_fs";
-                mountpoint = "/persist/system/cache";
-                options.mountpoint = "legacy";
-              };
-            };
-
-            homeDatasets = {
-              home = {
-                type = "zfs_fs";
-                options.mountpoint = "none";
-              };
-              "home/bphenriques" = {
-                type = "zfs_fs";
-                options.mountpoint = "none";
-              };
-              "home/bphenriques/documents" = {
-                type = "zfs_fs";
-                mountpoint = "/home/bphenriques/documents";
-                options.mountpoint = "legacy";
-              };
-              "home/bphenriques/persist" = {
-                type = "zfs_fs";
-                mountpoint = "/persist/bphenriques";
-                options.mountpoint = "legacy";
-              };
-              "home/bphenriques/cache" = {
-                type = "zfs_fs";
-                mountpoint = "/persist/bphenriques/cache";
-                options.mountpoint = "legacy";
-              };
-            };
-          in systemdDatasets // homeDatasets // data;
+      # https://www.high-availability.com/docs/ZFS-Tuning-Guide/#general-recommendations
+      rootFsOptions = {
+        compression = "lz4";
+        xattr = "sa";
+        atime = "off";
       };
-     };
+
+      options = {
+        # SSD supports 512/512 (physical/logical):
+        #   $ lsblk -t /dev/nvme0n1
+        #
+        # But.. it actually supports 4096/4096:
+        #   $ nvme id-ns -H /dev/nvme0n1
+        #
+        # 512/512 is mostly for compatability reasons and, in our case, we can set it by setting it up accordingly:
+        # $ nvme format --lbaf=1 /dev/nvme0n1
+        #
+        # https://wiki.archlinux.org/title/Advanced_Format
+        # https://www.high-availability.com/docs/ZFS-Tuning-Guide/#alignment-shift-ashiftn
+        ashift = "12";
+      };
+
+      # https://github.com/KornelJahn/nixos-disko-zfs-test/blob/main/hosts/testhost-disko.nix
+      datasets =
+        let
+          systemDatasets = {
+            system = {
+              type = "zfs_fs";
+              options.mountpoint = "none";
+            };
+            "system/root" = {
+              type = "zfs_fs";
+              mountpoint = "/";
+              options.mountpoint = "legacy";
+              postCreateHook = ''zfs snapshot zroot/system/root@blank'';
+            };
+            "system/nix" = {
+              type = "zfs_fs";
+              mountpoint = "/nix";
+              options.mountpoint = "legacy";
+            };
+            "system/persist" = {
+              type = "zfs_fs";
+              mountpoint = "/persist/system";
+              options.mountpoint = "legacy";
+            };
+            "system/cache" = {
+              type = "zfs_fs";
+              mountpoint = "/persist/system/cache";
+              options.mountpoint = "legacy";
+            };
+          };
+
+          homeDatasets = {
+            home = {
+              type = "zfs_fs";
+              options.mountpoint = "none";
+            };
+            "home/bphenriques" = {
+              type = "zfs_fs";
+              options.mountpoint = "none";
+            };
+            "home/bphenriques/documents" = {
+              type = "zfs_fs";
+              mountpoint = "/home/bphenriques/documents";
+              options.mountpoint = "legacy";
+            };
+            "home/bphenriques/persist" = {
+              type = "zfs_fs";
+              mountpoint = "/persist/bphenriques";
+              options.mountpoint = "legacy";
+            };
+            "home/bphenriques/cache" = {
+              type = "zfs_fs";
+              mountpoint = "/persist/bphenriques/cache";
+              options.mountpoint = "legacy";
+            };
+          };
+
+          dataDatasets = {
+            "data" = {
+              type = "zfs_fs";
+              mountpoint = "/mnt/data";
+              options.mountpoint = "legacy";
+            };
+          };
+
+        in systemDatasets // homeDatasets // dataDatasets;
     };
+   };
   };
 
   fileSystems = {
     "/".neededForBoot = true;
     "/nix".neededForBoot = true;
     "/boot".neededForBoot = true;
-    "/persist".neededForBoot = true;
+    "/persist/system".neededForBoot = true;
+    "/persist/system/cache".neededForBoot = true;
+    "/persist/bphenriques".neededForBoot = true;
+    "/persist/bphenriques/cache".neededForBoot = true;
   };
 }
