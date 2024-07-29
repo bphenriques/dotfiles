270346e21c26ef347154087a34a03f84a23e6547
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2024-07-29 10:57:58 +0100

    Make dotfiles paths parametrizable given impermeneance

--- apps/dotfiles-install.sh
+++ apps/dotfiles-install.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env sh
 
-# Constants
-DOTFILES_LOCATION="$HOME"/.dotfiles
-SSH_KEY_LOCATION="$HOME"/.ssh/id_ed25519.pub
-HOST_FILE_LOCATION="$DOTFILES_LOCATION/.nix-host"
-
 usage() {
-  echo "Setups the dotfiles repository under $DOTFILES_LOCATION
+  echo "Setups the dotfiles.
 
-Usage: dotfiles-installer.sh --host <host>
+Usage: dotfiles-installer.sh <host> [--dotfiles-location] [-ssh-directory] [--ssh-key-comment]
 
-  --host            name of the host under $DOTFILES_LOCATION/hosts/
+Arguments:
+  host                name of the host under the hosts folder
 
 Options:
-  --ssh-key-comment defaults to git user.email
+  --dotfiles-location defaults to \$HOME/.dotfiles
+  --ssh-directory     defaults to \$HOME/.ssh
+  --ssh-key-comment   defaults to git user.email
 "
 }
 
@@ -25,66 +23,78 @@ fatal() { printf '[\033[0;31mFAIL\033[0m] %s\n' "$1" 1>&2; exit 1; }
 press_to_continue() { info 'Press any key to continue'; read -r _; }
 
 clone_dotfiles() {
-  # Works for impermanence where the directory was mounted but it is empty
-  if ! test -d "$DOTFILES_LOCATION" || (find "$DOTFILES_LOCATION" -maxdepth 0 -empty | read -r _); then
-    info 'dotfiles - Cloning!'
-    git clone -b master git@github.com:bphenriques/dotfiles.git "$DOTFILES_LOCATION"
+  location="$dotfiles_location"
+  # Works for impermanence where the directory is mounted and empty
+  if ! test -d "$location" || (find "$location" -maxdepth 0 -empty | read -r _); then
+    info "dotfiles - Cloning to $location"
+    git clone -b master git@github.com:bphenriques/dotfiles.git "$location"
   fi
-  success 'dotfiles - Cloned!'
+  success "dotfiles - available in $location"
 }
 
 setup_ssh() {
-  comment="$1"
-  if test -z "$comment"; then
-    fail "SSH comment is empty"
-  fi
+  directory="$1"
+  comment="$2"
+  test -z "$comment"  && fatal "SSH comment is empty"
 
-  if [ ! -f "$SSH_KEY_LOCATION" ]; then
-    info 'SSH Key - Creating new key'
-    ssh-keygen -t ed25519 -C "$comment"
-    info "SSH Key - Copy the public key below to https://github.com/settings/ssh/new"
-    cat "$SSH_KEY_LOCATION"
+  echo "$directory"
+  mkdir -p "${directory}"
+  if [ ! -f "$directory"/id_ed25519.pub ]; then
+    info "SSH Key - Exporting key-pair on $directory"
+    ssh-keygen -t ed25519 -C "$comment" -f "$directory"/id_ed25519
+    info 'SSH Key - Copy the public key below to https://github.com/settings/ssh/new'
+    cat "$directory"/id_ed25519.pub
     echo ''
     press_to_continue
+  else
+    success "SSH Key - Key-pair already present in $directory"
   fi
-  success 'SSH Key - Created!'
+  success "SSH Key - Key-pair available in $directory"
 }
 
 setup_git_filter() {
   info 'Nix Evaluation Secrets - Checking...'
-  if ! "$DOTFILES_LOCATION"/bin/git-secret-filter.sh doctor; then
+  if ! "$dotfiles_location"/bin/git-secret-filter.sh doctor; then
     info "Nix Evaluation Secrets - Initialing and checking out to to smudge the secrets"
-    "$DOTFILES_LOCATION"/bin/git-secret-filter.sh init
+    "$dotfiles_location"/bin/git-secret-filter.sh init
     git checkout master
   fi
   success "Nix Evaluation Secrets - Done!"
 }
 
 validate_host() {
-  host="$1"
-  if test -z "${host}"; then
-    fatal "host must not be empty!"
-  elif ! test -d "${DOTFILES_LOCATION}/hosts/${host}"; then
-    fatal "No matchist '${host}' under '${DOTFILES_LOCATION}/hosts'"
-  elif [ -f "$HOST_FILE_LOCATION" ] && [ "$(cat "$HOST_FILE_LOCATION")" != "${host}" ]; then
-    fatal "There is already a host set that is not identical. Delete ${HOST_FILE_LOCATION} and try again."
+  dotfiles_location="$1"
+  host="$2"
+  host_file_location="$dotfiles_location/.nix-host"
+
+  test -z "${host}" && fatal "host must not be empty!"
+  ! test -d "${dotfiles_location}/hosts/${host}" && fatal "No matching '${host}' under '${dotfiles_location}/hosts'"
+  if [ -f "$host_file_location" ] && [ "$(cat "$host_file_location")" != "${host}" ]; then
+    fatal "There is already a host set that is not identical. Delete ${host_file_location} and try again."
   fi
   success 'Host - Valid!'
 }
 
+if [ "$1" == "--help" ]; then
+  usage
+  exit
+fi
+
+dotfiles_location="$HOME"/.dotfiles
+ssh_directory="$HOME"/.ssh
 ssh_key_comment="$(git config user.email)"
-host=
+
+host="$1"
+shift 1
 while [ $# -gt 0 ]; do
   case "$1" in
-    --help)             usage;                  exit  0 ;;
-    --ssh-key-comment)  ssh_key_comment="$2";   shift 2 ;;
-    --host)             host="$2";              shift 2 ;;
+    --dotfiles-location)  dotfiles_location="$2";   shift 2 ;;
+    --ssh-directory)      ssh_directory="$2";       shift 2 ;;
+    --ssh-key-comment)    ssh_key_comment="$2";     shift 2 ;;
     *) break ;;
   esac
 done
 
-setup_ssh "${ssh_key_comment}"
-clone_dotfiles
-validate_host "${host}"
-
-# TODO: run setup_git_filter iff there is a .age. file somewhere AND we have keys to decrypt
+setup_ssh "${ssh_directory}" "${ssh_key_comment}"
+clone_dotfiles "${dotfiles_location}"
+validate_host "${dotfiles_location}" "${host}"
