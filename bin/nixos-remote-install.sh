9b6a6ed529ad3b8a9d6f4b1ca2a040df9493eb42
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2024-08-13 16:04:17 +0100

    cleanup

--- bin/nixos-remote-install.sh
+++ bin/nixos-remote-install.sh
@@ -3,70 +3,50 @@
 #! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/6a8a86ede2e8ce496648f2a8ae79d2c24464ca2a.tar.gz
 # shellcheck shell=sh disable=SC2046,SC3028
 SCRIPT_PATH="$(dirname "$0")"
-
-# ./bin/nixos-remote-install.sh laptop nixos@192.168.68.59 --sops-age-destination /persist/data/home/bphenriques/.config/sops/age/keys.txt
-
 set -e
 
+DOTFILES_LOCATION="${SCRIPT_PATH}/.."
+
+
 usage() {
-  echo "Setups a nixos machine
+  echo "nixos-remote-install.sh {host} {ssh_host} [--sops-age-file file]
 
-Usage: nixos-installer.sh [build|install]
-
-  --host            matching directory name under  match on of the directories under $DOTFILES_LOCATION/hosts/
-
-Options:
+  --sops-age-file            target location of the age files containing the private keys.
 "
 }
 
 info() { printf '[ \033[00;34m..\033[0m ] %s\n' "$1"; }
-success() { printf '[ \033[00;32mOK\033[0m ] %s\n' "$1"; }
-warn() { printf '[ \033[01;33mWARN\033[0m ] %s\n' "$1"; }
-error() { printf '[\033[0;31mERROR\033[0m] %s\n' "$1" 1>&2; }
 fatal() { printf '[\033[0;31mFAIL\033[0m] %s\n' "$1" 1>&2; exit 1; }
-press_to_continue() { info 'Press any key to continue'; read -r _; }
 
 host_require_secrets() {
-  host="$1"
-  dotfiles_location="$2"
-
-  yq '.keys[] | anchor' < "${dotfiles_location}"/.sops.yaml | grep -E "^${host}$" > /dev/null 2>&1;
+  yq '.keys[] | anchor' < "${DOTFILES_LOCATION}"/.sops.yaml | grep -E "^$1$" > /dev/null 2>&1;
 }
 
-fetch_age_private_key() {
-  bw get item "sops-age-key-$1" | jq --raw-output '.fields[] | select(.name=="private") | .value'
-}
+if [ $# -lt 3 ] || [ "$1" == "--help" ]; then
+  usage
+  exit 1
+fi
 
-dotfiles_location="$HOME"/.dotfiles
 host="$1"
 ssh_host="$2"
-shift 2
 
-sops_age_destination=
+sops_age_file=
 while [ $# -gt 0 ]; do
   case "$1" in
-    --help)                     usage;                          exit  0 ;;
-    --sops-age-destination)     sops_age_destination="$2";      shift 2 ;;
+    --sops-age-file)     sops_age_file="$2";      shift 2 ;;
     *) break ;;
   esac
 done
-test -z "${host}" && error "host is not set!" && usage && exit 1
-! test -d "${dotfiles_location}/hosts/${host}" && fatal "No matching '${host}' under '${dotfiles_location}/hosts'"
-test -z "${ssh_host}" && error "ssh_host is not set!" && usage && exit 1
-test -z "${BW_SESSION}" && error "BW_SESSION is not set. Unlock your bitwarden vault to continue." && usage && exit 1
+! test -d "${DOTFILES_LOCATION}/hosts/${host}" && fatal "No matching '${host}' under '${DOTFILES_LOCATION}/hosts'"
 
-file_tree_to_copy="$(mktemp -d)"
-if host_require_secrets "$host" "${dotfiles_location}"; then
-  test -z "${sops_age_destination}" && error "sops-age-destination argument is required as '${host}' requires secrets!" && usage && exit 1
+EXTRA_FILES="$(mktemp -d)"
+if host_require_secrets "$host"; then
+  test -z "${sops_age_file}" && error "sops-age-file argument is required as '${host}' requires secrets!" && usage && exit 1
 
   info "'${host}' contains secrets. Setting private keys from Bitwarden"
   bw unlock --check > /dev/null || fatal "Vault must be unlocked"
-  mkdir -p "$(dirname "${file_tree_to_copy}/${sops_age_destination}")"
-  fetch_age_private_key "${host}" > "${file_tree_to_copy}/${sops_age_destination}"
-  # TODO: should I run this only after the file is copied?
-  #chmod 600 "${file_tree_to_copy}/${sops_age_destination}"
-else
-  info "${host} does not require secrets."
+  mkdir -p "$(dirname "${EXTRA_FILES}/${sops_age_file}")"
+  bw get item "sops-age-key-${host}" | jq --raw-output '.fields[] | select(.name=="private") | .value' > "${EXTRA_FILES}/${sops_age_file}"
 fi
 
-nix run github:nix-community/nixos-anywhere -- --extra-files "$file_tree_to_copy" --flake ".#${host}" "${ssh_host}"
+nix run github:nix-community/nixos-anywhere -- --extra-files "$EXTRA_FILES" --flake ".#${host}" "${ssh_host}"
