0ff86a4cb989e692741723cc68d80b6f1b69ab7c
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2024-08-23 16:02:26 +0100

    Major review of the permissions

--- config/home-manager/misc/input-remapper/default.nix
+++ config/home-manager/misc/input-remapper/default.nix
@@ -2,7 +2,7 @@
 {
   # Workaround to readonly FS: https://github.com/sezanzeb/input-remapper/issues/663
   xdg.configFile = {
-    "input-remapper/config.json".source = config.lib.file.mkOutOfStoreSymlink "${config.custom.dotfiles.directory}/home/input-remapper/config.json";
-    "input-remapper/presets/Logitech G305/Media.json".source = config.lib.file.mkOutOfStoreSymlink "${config.custom.dotfiles.directory}/home/input-remapper/Media.json";
+    "input-remapper/config.json".source = config.lib.file.mkOutOfStoreSymlink "${config.custom.dotfiles.directory}/home-manager/misc/input-remapper/config.json";
+    "input-remapper/presets/Logitech G305/Media.json".source = config.lib.file.mkOutOfStoreSymlink "${config.custom.dotfiles.directory}/home-manager/misc/input-remapper/Media.json";
   };
 }
