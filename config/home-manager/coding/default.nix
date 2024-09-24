c08d586646c93c3bddeb2a441a397e76dc9eed28
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2024-09-24 20:00:02 +0100

    iteration

--- config/home-manager/coding/default.nix
+++ config/home-manager/coding/default.nix
@@ -2,20 +2,11 @@
 {
   imports = [ ./git ];
 
-  # https://github.com/0xcharly/nix-config/blob/a8e1427a67494ad5de3d639d94ee619ca69f51c7/users/delay/home.nix#L99 ?
+  # TODO: Explore jujutsu: https://github.com/0xcharly/nix-config/blob/a8e1427a67494ad5de3d639d94ee619ca69f51c7/users/delay/home.nix#L99 ?
 
   # Some of these packages should likely move to shell environment
   home.packages = with pkgs; [
-    # Need to get familiar with working with it....
-    jetbrains.idea-community # TODO Add plugins: GraphQL, "Makefile Language" , Scala, Terraform and HCL,
-
-    # Linters
-    shellcheck        # Linter for shell scripts.
-    shfmt             # Format shell scripts
-    nixfmt-rfc-style  # Format nix files.
-  ] ++ lib.optionals pkgs.stdenv.isDarwin [
-    lima    # Virtual Machine -  limactl start --set='.cpus = 4 | .memory = "10GiB"'
-    (pkgs.writeShellScriptBin "docker" ''${lima}/bin/lima nerdctl $@'')
+    jetbrains.idea-community
   ] ++ lib.optionals (pkgs.stdenv.isLinux && !headless) [
     filezilla   # Access remote files
   ];
---
+++ config/home-manager/desktop/wofi.nix
@@ -0,0 +1,72 @@
+{ config, lib, pkgs, headless, ... }:
+let
+  font = {
+    variable = "Hack Nerd Font Mono";
+  };
+  colorScheme = {
+    palette = {
+      base00 = "#000000";
+      base01 = "#100323";
+      base02 = "#3C3C3C";
+      base03 = "#595959";
+      base04 = "#BEBCBF";
+      base05 = "#FFFFFF";
+      base06 = "#EDEAEF";
+      base07 = "#FFFFFF";
+      base08 = "#FF8059";
+      base09 = "#EF8B50";
+      base0A = "#D0BC00";
+      base0B = "#44BC44";
+      base0C = "#00D3D0";
+      base0D = "#2FAFFF";
+      base0E = "#FEACD0";
+      base0F = "#B6A0FF";
+    };
+  };
+in
+{
+  programs.wofi = {
+    enable = pkgs.stdenv.isLinux && !headless;
+    settings = {
+      width = 800;
+      height = 400;
+      insensitive = true;
+      mode = "drun,run";
+    };
+
+    style = ''
+      window {
+        border: 2px solid #${colorScheme.palette.base03};
+        background-color: #${colorScheme.palette.base00};
+      }
+
+      #input {
+        color: #${colorScheme.palette.base09};
+        border: 2px solid #${colorScheme.palette.base03};
+        background-color: #${colorScheme.palette.base00};
+        font-size: 13px;
+        font-family: ${font.variable};
+      }
+
+      #outer-box {
+        margin: 10px;
+      }
+
+      #scroll {
+        margin: 5px 0px;
+        font-size: 13px;
+        font-family: ${font.variable};
+        color: #${colorScheme.palette.base06};
+      }
+
+      #scroll label {
+        margin: 2px 0px;
+      }
+
+      #entry:selected {
+        color: #${colorScheme.palette.base06};
+        background-color: #${colorScheme.palette.base00};
+      }
+    '';
+  };
+}
