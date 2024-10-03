3fda73f7c90ef7bde30e0557ae7c8216634eb561
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2024-10-03 17:38:33 +0100

    Fix macos installation

--- hosts/work-macos/default.nix
+++ hosts/work-macos/default.nix
@@ -1,11 +1,10 @@
-{ self, mylib, nixpkgs, darwinModules, home-manager, ... }:
+{ self, mylib, nixpkgs, home-manager, ... }:
 let
   inherit (nixpkgs.lib.attrsets) attrValues;
 
-  overlays = attrValues self.overlays ++ [ nur.overlay ];
   darwinModules = attrValues self.darwinModules ++ [ home-manager.darwinModules.home-manager ];
   hmModules = attrValues self.homeManagerModules;
 in mylib.hosts.mkMacOSHost {
-  inherit darwinModules hmModules overlays;
+  inherit darwinModules hmModules;
   hostModule = ./config.nix;
 }
