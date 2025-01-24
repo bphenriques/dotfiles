3acebdd4ec421ca600044c1a739bdc2b9666cb2a
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-01-24 04:44:16 +0000

    Simplify fish+brew in macos using system fish config

--- darwin/default.nix
+++ darwin/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   imports = [
     ./brew.nix
@@ -6,19 +6,17 @@
     ./preferences.nix
   ];
 
-  services.nix-daemon.enable = true;
-
-  home-manager.useGlobalPkgs   = true;   # Use pkgs set within nixpkgs.
-  home-manager.useUserPackages = true;   # Install packages defined in home-manager.
-
-  environment.systemPackages = [ pkgs.fish ]; # Install the shell
-  environment.shells = [ pkgs.fish ];         # Register the shell that was installed (2 step process)
-
-  system.includeUninstaller = false; # use 'nix run github:LnL7/nix-darwin#darwin-uninstaller'
+  programs.fish.enable = true;
+  environment.shells = [ config.programs.fish.package ];  # Register the shell
 
   # Fonts (system-wide)
   fonts.packages = with pkgs; [
     pkgs.nerd-fonts.hack
     pkgs.nerd-fonts.jetbrains-mono
   ];
+
+  # Misc
+  home-manager.useGlobalPkgs   = true;   # Use pkgs set within nixpkgs.
+  home-manager.useUserPackages = true;   # Install packages defined in home-manager.
+  system.includeUninstaller = false;     # use 'nix run github:LnL7/nix-darwin#darwin-uninstaller'
 }
