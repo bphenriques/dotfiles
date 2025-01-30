359e511d14d9276a6faa3d2e9eb6ae649ebeceff
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-01-30 13:08:00 +0000

    minor

--- home-manager/desktop/umu-launcher.nix
+++ home-manager/desktop/umu-launcher.nix
@@ -5,31 +5,26 @@ let
     text = ''GAMEID=0 ${lib.getExe pkgs.umu-run}'';
   };
 
-  mimeTypes = [
+  mimeType = [
     "application/x-ms-dos-executable"
     "application/x-msi"
     "application/x-ms-shortcut"
     "application/vnd.microsoft.portable-executable"
   ];
 
-  umu-run-default-desktop =
-    (pkgs.makeDesktopItem {
-      exec = "${lib.getExe umu-run0} %f";
-      name = "Umu Default Launcher";
-      type = "Application";
-      desktopName = "Umu Default Launcher";
-      categories = [ "Utility" "Game" ];
-      icon = "wine";
-      mimeTypes = mimeTypes;
-    });
-
-  setDefault = types: target: foldl' (acc: type: acc // { "${type}" = target; }) { } types;
+  setDefault = types: target: builtins.listToAttrs (mime: { "${mime}" = target; }) types;
 in lib.mkIf pkgs.stdenv.isLinux {
   home.packages = [
     umu-run0
     pkgs.umu-launcher
-    umu-run-default-desktop
   ];
 
-  xdg.mime.defaultApplications = setDefault mimeTypes "Umu Default Launcher.desktop";
+  xdg = {
+    desktopEntries."Umu Default Launcher" = {
+      inherit mimeType;
+      exec = "${lib.getExe umu-run0} %f";
+      icon = "wine";
+    };
+    mime.defaultApplications = setDefault mimeTypes "Umu Default Launcher.desktop";
+  };
 }
