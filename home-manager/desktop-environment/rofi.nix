34e4128a0ea8ade5d07d7f4bcc5aad6d123c052a
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-02-10 15:48:23 +0000

    Add rofi plugins and add notes

--- home-manager/desktop-environment/rofi.nix
+++ home-manager/desktop-environment/rofi.nix
@@ -1,6 +1,5 @@
 { pkgs, lib, ... }:
 
-# desktop items: https://github.com/edmundmiller/dotfiles/blob/main/modules/desktop/apps/rofi.nix
 {
   stylix.targets.rofi.enable = true;
 
@@ -8,12 +7,27 @@
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
+    plugins = [
+      pkgs.rofi-emoji-wayland
+      (pkgs.rofi-calc.override { rofi-unwrapped = pkgs.rofi-wayland-unwrapped; })
+    ];
   };
 }
 
-# https://github.com/iynaix/dotfiles/blob/e441ab4ff7a775b57b6c79a2fa6be99e3ab2d58b/home-manager/programs/rofi.nix
-# https://github.com/iynaix/dotfiles/blob/e441ab4ff7a775b57b6c79a2fa6be99e3ab2d58b/home-manager/programs/rofi-wifi-menu.sh
+# rofi -modi calc -show calc -no-show-match -no-sort
+# rofi -modi emoji -show emoji
+
+# https://github.com/71zenith/kiseki/blob/2b85b44338b369a4d22baae2e684fa783e64afc2/home/rofi.nix#L248
+
 # https://github.com/iynaix/dotfiles/blob/e441ab4ff7a775b57b6c79a2fa6be99e3ab2d58b/home-manager/programs/rofi-power-menu.sh
 # https://github.com/adi1090x/rofi/tree/master/files
 # https://github.com/edmundmiller/dotfiles/blob/main/modules/desktop/apps/rofi.nix
-# https://github.com/edmundmiller/dotfiles/blob/main/config/rofi/bin/rofi-browsermenu
+# desktop items: https://github.com/edmundmiller/dotfiles/blob/main/modules/desktop/apps/rofi.nix
+
+
+# System monitor with:
+# - Time
+# - Battery (if applicable) (and potentially set the power profile)
+# - Volume
+# - Network
+# - Keyboard
