a9fd77f7c3d84667f015a1e493d1e444382b903b
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-03-10 18:04:30 +0000

    bump

--- home-manager/yazi.nix
+++ home-manager/yazi.nix
@@ -1,10 +1,5 @@
 { pkgs, config, lib, ... }:
-let
-  downloadDirName = "downloads";
-in
 {
-  # TODO: https://github.com/bigbabyboost/dotfiles/blob/hyprnix/home/terminal/programs/yazi/default.nix
-  # Default: https://github.com/sxyazi/yazi/blob/shipped/yazi-config/preset/yazi-default.toml
   stylix.targets.yazi.enable = true;
   programs.yazi = {
     enable = true;
@@ -29,36 +24,6 @@ in
     theme.filetype.rules = lib.mkBefore [
       { mime = "*"; is = "orphan"; bg = "red"; }        # Highlight orphaned files
     ];
-  };
-
-  xdg.configFile = {
-    # TODO: Migrate to hm types
-    # Smart sorting depending on the directory
-    "yazi/plugins/folder-rules.yazi/main.lua".text = ''
-      local function setup()
-        ps.sub("cd", function()
-          local cwd = cx.active.current.cwd
-          if cwd:ends_with("${downloadDirName}") then
-            ya.manager_emit("sort", { "modified", reverse = true, dir_first = false })
-          else
-            ya.manager_emit(
-              "sort",
-              {
-                sort_by = "${config.programs.yazi.settings.manager.sort_by}",
-                reverse = false,
-                dir_first = ${toString config.programs.yazi.settings.manager.sort_dir_first}
-              }
-            )
-          end
-        end)
-      end
-
-      return { setup = setup }
-    '';
-
-    "yazi/init.lua".text = ''
-      require("folder-rules"):setup()
-    '';
   };
 
   systemd.user.tmpfiles.rules = lib.optionals pkgs.stdenv.isLinux [
