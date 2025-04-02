394b7ba148c172cbb7d45423e9e110056b491f39
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-04-02 12:52:03 +0100

    WIP

--- hosts/laptop/install.sh
+++ hosts/laptop/install.sh
@@ -1,41 +1,41 @@
 # Run:
-# nix-shell -p https://github.com/bphenriques/dotfiles#bw-session
+# nix profile install github:bphenriques/dotfiles/wayland-move-btrfs#bw-session
 
 BRANCH_NAME="${BRANCH_NAME:-main}"
 FLAKE_URL="${FLAKE_URL:-github:bphenriques/dotfiles/${BRANCH_NAME}}"
 SOPS_AGE_SYSTEM_FILE="/var/lib/sops-nix/system-keys.txt"
 
-info() { printf '[ \033[00;34m  \033[0m ] %s\n' "$1"; }
-error() { printf '[\033[0;31mERROR\033[0m] %s\n' "$1" 1>&2; }
-fatal() { printf '[\033[0;31mFAIL\033[0m] %s\n' "$1" 1>&2; exit 1; }
+fatal() { printf '%s\n' "$1" 1>&2; exit 1; }
 
 check_bitwarden_unlocked() {
   if [ -z "$BW_SESSION" ]; then
-    BW_SESSION="$(bw-session session "${bw_email}")"
+    BW_SESSION="$(bw-session session "$1")"
     export BW_SESSION
   fi
 
-  bw unlock --check > /dev/null || fatal "Vault must be unlocked"
+  bw-session check > /dev/null || fatal "Vault must be unlocked"
 }
 
+check_bitwarden_unlocked "$1"
+
 # Authentication - I have private flakes, therefore need to set the Github token
-info "Fetching Github credentials..."
+echo "Fetching Github credentials..."
 export GITBUB_TOKEN="$(nix run .#bw-session -- get-item-field "Github Token" "token")"
 export NIX_CONFIG="access-tokens = github.com=$GITHUB_TOKEN"
 
 # Has to respect disko.nix
-info "Fetching Luks keys..."
+echo "Fetching Luks keys..."
 nix run .#bw-session -- get-item-field "system-nixos-laptop" luks-main-key    > "/tmp/luks-main.key"
 nix run .#bw-session -- get-item-field "system-nixos-laptop" luks-backup-key  > "/tmp/luks-backup.key"
 
 # Sops
-info "Fetching Sops private keys..."
+echo "Fetching Sops private keys..."
 bw-session get-item-field "system-nixos-laptop" "sops-system-private-key" > "/tmp/system-keys.txt"
 
 info "Running Disko..."
-sudo nix run 'github:nix-community/disko/latest#disko-install' -- \
-  --flake "${FLAKE_URL}#${host}" \
-  --disk "${disk_name}" "${disk_device}" \
-  --extra-files /tmp/system-keys.txt "${SOPS_AGE_SYSTEM_FILE}"
-  --dry-run
+#sudo nix run 'github:nix-community/disko/latest#disko-install' -- \
+#  --flake "${FLAKE_URL}#${host}" \
+#  --disk "${disk_name}" "${disk_device}" \
+#  --extra-files /tmp/system-keys.txt "${SOPS_AGE_SYSTEM_FILE}"
+#  --dry-run
 
