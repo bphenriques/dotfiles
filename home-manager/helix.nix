2bfae747858296df601df50170b5a93b503413b7
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-04-02 18:24:42 +0100

    Update helix.nix

--- home-manager/helix.nix
+++ home-manager/helix.nix
@@ -4,7 +4,7 @@
   programs.helix = {
     enable = true;
     defaultEditor = true;
-    package = pkgs.helix-git;
+    # package = pkgs.helix-git;
 
     # Language Server Protocols. Check with hx --health
     extraPackages = [
