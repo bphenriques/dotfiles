b9e849e795cfcce54ea418504746b53cdfb177ea
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-04-04 10:39:28 +0100

    Fine tuning

--- modules/nixos/hardware/8bitdo-ultimate2c.nix
+++ modules/nixos/hardware/8bitdo-ultimate2c.nix
@@ -1,19 +1,13 @@
 { pkgs, config, lib, ... }:
 let
   cfg = config.custom.hardware.gamepad-8bitdo-ultimate2c;
+
+  # Discoverable using lsusb. You will find 2dc8:310a.
+  vendorId = "2dc8";
+  productId = "310a";
 in {
   options.custom.hardware.gamepad-8bitdo-ultimate2c = with lib.types; {
     enable = lib.mkEnableOption "8bitdo 2.4 Ultimate 2C Controller support";
-
-    vendorId = lib.mkOption {
-      type = str;
-      description = "id of the product. You can find using lsusb: ID 2dc8:310a means 2dc8 as vendorId";
-    };
-
-    productId = lib.mkOption {
-      type = str;
-      description = "id of the product. You can find using lsusb: ID 2dc8:310a means 310a as productId";
-    };
   };
 
   # Tip: When on: press and hold X/B and Home to power off. Then hold X/B to power on. The mode will be the one selected:
@@ -21,12 +15,14 @@ in {
   # - D -> DInput (displays as 8Bitdo Ultimate 2C Wireless)
   config = lib.mkIf cfg.enable {
     hardware.xpadneo.enable = true;   # Wireless Xbox(ish) gamepads (e.g., 8bitdo)
+
+    # FIXME: This should be done automatically by Kernel 6.12
     services.udev.extraRules = ''
       ACTION=="add", \
-        ATTRS{idVendor}=="${cfg.vendorId}", \
-        ATTRS{idProduct}=="${cfg.productId}", \
+        ATTRS{idVendor}=="${vendorId}", \
+        ATTRS{idProduct}=="${productId}", \
         RUN+="${pkgs.kmod}/bin/modprobe xpad", \
-        RUN+="${pkgs.bash}/bin/sh -c 'echo ${cfg.vendorId} ${cfg.productId} > /sys/bus/usb/drivers/xpad/new_id'"
+        RUN+="${pkgs.bash}/bin/sh -c 'echo ${vendorId} ${productId} > /sys/bus/usb/drivers/xpad/new_id'"
     '';
   };
 }
