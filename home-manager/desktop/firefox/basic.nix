4ed7ecb1795d10fd8b9212ce5bf34c398b1f361a
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2025-04-07 14:27:00 +0100

    WIP

--- home-manager/desktop/firefox/basic.nix
+++ home-manager/desktop/firefox/basic.nix
@@ -16,7 +16,7 @@
   "browser.toolbars.bookmarks.visibility" = "always";           # Always show the toolbar.
   "extensions.autoDisableScopes" = "0";                         # Automatically enable extensions
   "signon.rememberSignons" = false;                             # No thank you.
-
+  "extensions.autoDisableScopes" = 0;                           # Automatically enable extensions
 
   # Extra Security
   "privacy.globalprivacycontrol.functionality.enabled" = true;  # Private as much as possible.
