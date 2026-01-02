ed2c5bb127ce8388d3fdc1c754dcc4a9427d8ab8
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2026-01-02 23:03:27 +0000

    WIP

--- hosts/compute/hardware/default.nix
+++ hosts/compute/hardware/default.nix
@@ -4,6 +4,19 @@
     ./hardware-configuration.nix  # Output of nixos-generate-config --root /mnt
   ];
 
+  # Misc drivers
+  hardware.enableRedistributableFirmware = true;
+
+  # GPU
+  #hardware.graphics = {
+  #  enable = true;
+  #  extraPackages = with pkgs; [
+  #    intel-media-driver # LIBVA_DRIVER_NAME=iHD
+  #    intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but good fallback)
+  #    vaapiVdpau
+  #    libvdpau-va-gl
+  #  ];
+  #};
   # TODO: disable wifi card to lower power usage.
 
 }
