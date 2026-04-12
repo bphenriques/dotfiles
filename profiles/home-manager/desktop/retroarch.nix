861e4742fb51684dd6db3117560237c8c83a6848
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2026-04-12 18:53:34 +0100

    Add retroarch core options

--- profiles/home-manager/desktop/retroarch.nix
+++ profiles/home-manager/desktop/retroarch.nix
@@ -2,6 +2,54 @@
 let
   emulationPaths = osConfig.custom.homelab.paths.media.gaming.emulation;
   stateDir = "${config.xdg.stateHome}/retroarch";
+
+  # Per-core options: https://docs.libretro.com/library/
+  # Key names verified against libretro source code.
+  # Directory name must match core's display name exactly.
+  coreOptions = {
+    "Genesis Plus GX" = ''
+      genesis_plus_gx_overscan = "disabled"
+      genesis_plus_gx_render = "single field"
+    '';
+    "FCEUmm" = ''
+      fceumm_overscan_h = "enabled"
+      fceumm_overscan_v = "enabled"
+    '';
+    "SwanStation" = ''
+      swanstation_GPU_ResolutionScale = "3"
+      swanstation_GPU_TrueColor = "true"
+      swanstation_GPU_TextureFilter = "Nearest"
+      swanstation_GPU_PGXPEnable = "true"
+      swanstation_GPU_PGXPCulling = "true"
+      swanstation_GPU_PGXPTextureCorrection = "true"
+    '';
+    "Flycast" = ''
+      flycast_internal_resolution = "1920x1440"
+      flycast_anistropic_filtering = "4"
+      flycast_enable_rtt = "On"
+    '';
+    "DeSmuME" = ''
+      desmume_internal_resolution = "512x384"
+      desmume_screens_layout = "top/bottom"
+      desmume_screens_gap = "0"
+    '';
+    "Gambatte" = ''
+      gambatte_gb_colorization = "internal"
+      gambatte_gb_internal_palette = "GB - DMG"
+      gambatte_gbc_color_correction = "GBC only"
+      gambatte_gbc_color_correction_mode = "accurate"
+      gambatte_gbc_frontlight_position = "central"
+      gambatte_mix_frames = "accurate"
+      gambatte_dark_filter_level = "0"
+    '';
+    "mGBA" = ''
+      mgba_interframe_blending = "mix"
+    '';
+  };
+
+  coreOptionFiles = lib.mapAttrs' (name: text:
+    lib.nameValuePair "retroarch/config/${name}/${name}.opt" { inherit text; }
+  ) coreOptions;
 in
 lib.mkIf pkgs.stdenv.isLinux {
   programs.retroarch = {
@@ -38,11 +86,16 @@ lib.mkIf pkgs.stdenv.isLinux {
 
       # Video
       video_driver = "vulkan";
+      video_scale_integer = "true";  # Automatic integer scale
+      aspect_ratio_index = "22";     # "Core Provided": let each core report its native ratio
+      video_smooth = "false";        # Nearest-neighbor for sharp pixels
 
       # Prevent RetroArch from overwriting managed config
       config_save_on_exit = "false";
     };
   };
+
+  xdg.configFile = coreOptionFiles;
 
   home.packages = [
     pkgs.mame-tools  # Convert to CHD: parallel chdman createcd -i {} -o {.}.chd ::: *.iso
